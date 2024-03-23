b4d0f23445597c13bc071be78ffb102b1add56a2
SinethMendis <mendissineth03@gmail.com>
2024-03-23 17:27:33 +0530

    Added the displayAllBooks function to display the book details on individual cards

--- script.js
+++ script.js
@@ -1,45 +1,85 @@
-const myLibrary = [];
+document.addEventListener("DOMContentLoaded", function () {
 
-class Book {
-    constructor(ISBN, title, author, pageCount, publishedYear, read) {
-        this.ISBN = ISBN;
-        this.title = title;
-        this.author = author;
-        this.pageCount = pageCount;
-        this.publishedYear = publishedYear;
-        this.read = read;
+    const myLibrary = [];
+    const cardContainer = document.querySelector(".your-projects-body")
+
+
+
+    class Book {
+        constructor(ISBN, title, author, pageCount, publishedYear, read) {
+            this.ISBN = ISBN;
+            this.title = title;
+            this.author = author;
+            this.pageCount = pageCount;
+            this.publishedYear = publishedYear;
+            this.read = read;
+        }
     }
-}
-
-Book.prototype.aboutBook = function () {
-    return `ISBN : ${this.ISBN}\nTitle : ${this.title}\nAuthor : ${this.author}\nPages : ${this.pageCount}\nPublished year : ${this.publishedYear}\nRead status : ${this.read}`
-}
-
-function addBookToLibrary(Book) {
-    myLibrary.push(Book);
-}
-
-function displayBook(ISBN) {
-    for (const book of myLibrary) {
-        // to display a specified book 
-        /* if (book.ISBN == ISBN) {
-            console.log(book)
-        } */
-        console.log(book)
-
+    Book.prototype.aboutBook = function () {
+        return `ISBN : ${this.ISBN}\nTitle : ${this.title}\nAuthor : ${this.author}\nPages : ${this.pageCount}\nPublished year : ${this.publishedYear}\nRead status : ${this.read}`
     }
-}
 
-const Book1 = new Book(1, "A1", "B1", 30, 2000, "yes")
-const Book2 = new Book(2, "A2", "B2", 30, 2000, "yes")
-const Book3 = new Book(3, "A3", "B3", 30, 2000, "yes")
-
-addBookToLibrary(Book1)
-addBookToLibrary(Book2)
-addBookToLibrary(Book3)
+    function addBookToLibrary(Book) {
+        myLibrary.push(Book);
+    }
 
 
-// console.log(Book1.aboutBook());
-// console.log(Book1);
-// console.log(myLibrary)
-displayBook();
+    function displayAllBooks() {
+        for (const book of myLibrary) {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            cardContainer.appendChild(card)
+            const verticalStroke = document.createElement("div")
+            verticalStroke.classList.add("vertical-stroke")
+            card.appendChild(verticalStroke)
+            const actions = document.createElement("div")
+            actions.classList.add("actions")
+            card.appendChild(actions)
+            const action1 = document.createElement("div")
+            const action2 = document.createElement("div")
+            const action3 = document.createElement("div")
+            action1.classList.add("action1")
+            action2.classList.add("action2")
+            action3.classList.add("action3")
+            actions.appendChild(action1)
+            actions.appendChild(action2)
+            actions.appendChild(action3)
+
+            const cardHeading = document.createElement("div")
+            cardHeading.classList.add("card-heading")
+            card.appendChild(cardHeading)
+            const cardPara = document.createElement("div")
+            cardPara.classList.add("card-para")
+            card.appendChild(cardPara)
+
+            cardHeading.textContent = book.title;
+            cardPara.innerHTML = `Author: ${book.author}<br>Pages: ${book.pageCount}<br>Published year: ${book.publishedYear}<br>Read: ${book.read}`;
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+    const Book1 = new Book(1, "A1", "B1", 30, 2000, "yes")
+    const Book2 = new Book(2, "A2", "B2", 30, 2000, "yes")
+    const Book3 = new Book(3, "A3", "B3", 30, 2000, "No")
+    
+
+    addBookToLibrary(Book1)
+    addBookToLibrary(Book2)
+    addBookToLibrary(Book3)
+
+    displayAllBooks();
+
+
+})
+
+
